de25e7b54862858c77cea38481259ece47e0e6ea
Nishant <thenishant3@gmail.com>
2018-06-24 13:58:30 +0530

    updated gitignore

--- src/main/java/tests/LoginTest.java
+++ src/main/java/tests/LoginTest.java
@@ -1,10 +1,8 @@
 package tests;
 
-import org.glassfish.grizzly.compression.lzma.impl.Base;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.LoginPage;
-import sun.rmi.runtime.Log;
 
 public class LoginTest extends BaseTest {
 
